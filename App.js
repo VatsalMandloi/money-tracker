f56222159c1e8715794554cb81d3f9b033e8da5e
VatsalMandloi <68588254+VatsalMandloi@users.noreply.github.com>
2022-05-13 01:10:00 +0530

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+         <StatusBar 
+        animated= 'true'
+        backgroundColor='#ff5757'
+        barStyle='dark-content'
+      />
     </View>
   );
 }
