af51c0bbdef92d21350bf5b56983bf687e81b22d
VatsalMandloi <68588254+VatsalMandloi@users.noreply.github.com>
2022-05-14 23:06:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+  
+  <View>Side Nav </View>
+
       <Text>Open up App.js to start working on your app!</Text>
          <StatusBar 
         animated= 'true'
