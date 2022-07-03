4a5f80f892c9d796d71029d2f7beba2c8a43ee6b
VatsalMandloi <vatsal.mandloi.19@gmail.com>
2022-07-03 23:39:50 +0530

    chat area

--- App.js
+++ App.js
@@ -9,10 +9,11 @@ import {
   Pressable,
   FlatList,
   Alert,
+  StatusBar,
 } from "react-native";
 import Constants from "expo-constants";
 import * as SQLite from "expo-sqlite";
-import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar";
+
 
 
 function openDatabase() {
@@ -38,31 +39,16 @@ export default function App() {
   const [desc, setDesc] = useState(null);
   const [amount, setAmount] = useState(null);
   const [accounts, setaccounts] = useState(null);
-  const [acc, setacc] = useState([{ id: "1", name: "va" },
-  {
-    id: 4,
-    name: 'non esse culpa molestiae omnis sed optio'
-  },
-  {
-    id: 5,
-    name: 'eaque aut omnis a'
-  },
-  {
-    id: 6,
-    name: 'natus impedit quibusdam illo est'
-  },]);
-  const [transaction, setTransaction] = useState(null);
-  
+  const [transactions, setTransactions] = useState(null);
   const [title, setTitle] = useState("👻");
   const [tpay, setTpay] = useState(null);
   const [treceive, setTreceive] = useState(null);
   const [total, setTotal] = useState(null);
   const [date, setDate] = useState(null);
-  const [forceUpdate, forceUpdateId] = useForceUpdate();
   const [modalVisible, setmodalVisibal] = useState(false);
   const [tranModalVisible, setTranModalVisible] = useState(false);
   const [side, setSide] = useState(null);
-  const [color, setColor] = useState(null);
+  const [selectedID, setSelID] = useState(null);
   const pay = 1;
   const receive = 0;
   
@@ -72,7 +58,7 @@ export default function App() {
         "create table if not exists accounts (id integer primary key not null, name text, tpay real, treceive real);"
       );
       tx.executeSql(
-        "create table if not exists transactions(id integer primary key not null, accountid integer, desc text, date date, amount real, side int)",[], (tx, results) => {
+        "create table if not exists tansactions(id integer primary key not null, accountid integer, desc text, date date, amount real, side int)",[], (tx, results) => {
           console.log("table created")
         }
       );
@@ -94,18 +80,15 @@ const add = (text) => {
      
      db.transaction((tx) => {
       tx.executeSql(
-        "insert into accounts(name, tpay, treceive) values(?, 0, 0)", [text], (tx, results) => {
-          
-        }
-      );
+        "insert into accounts(name, tpay, treceive) values(?, 0, 0)", [text]);
       tx.executeSql("select * from accounts", [], (tx, results) => {
         setaccounts(results.rows._array)
       }
       
       );
         },
-        null,
-        forceUpdate,
+        
+        
       );
         
  
@@ -133,9 +116,7 @@ const add = (text) => {
         setTpay(pay)
         db.transaction(
           (tx) => {
-            tx.executeSql("update accounts set tpay = ?  where id = ?", [pay, id], (tx,  result )=> {
-              console.log("updatep"+id+pay)
-          });
+            tx.executeSql("update accounts set tpay = ?  where id = ?", [pay, id]);
           }
         )
       } else {
@@ -144,27 +125,26 @@ const add = (text) => {
         setTreceive(pay);
         db.transaction(
           (tx) => {
-            tx.executeSql("update accounts set treceive = ?  where id = ?", [pay, id], (tx,  result )=> {
-              console.log("updater"+id+pay)
-          });
+            tx.executeSql("update accounts set treceive = ?  where id = ?", [pay, id]);
           }
         )
        
       }
-console.log(r+p)
+
       setTotal(r - p)
       
       const date = new Date().getDate();
       db.transaction(
         (tx) => {
-          tx.executeSql("insert into transactions(accountid, desc, date, amount, side) values (?, ?, ?, ?, ?)", [id, desc, date, amount, side]);
+          tx.executeSql("insert into tansactions(accountid, desc, date, amount, side) values (?, ?, ?, ?, ?)", [id, desc, date, amount, side]);
+         
           
+          dbTran(id)
         }
       )
       setTranModalVisible(!tranModalVisible);
     }
   };
- 
 
   const updateMain = (account) => {
     setId(account.id)
@@ -172,29 +152,46 @@ console.log(r+p)
     setTpay(account.tpay)
     setTreceive(account.treceive)
     setTotal(account.treceive-account.tpay)
+    dbTran(account.id)
+    setSelID(account.id)
+  }
+
+  const dbTran = (id) => {
     db.transaction(
       (tx) => {
-        tx.executeSql("select * from transactions where accountid = ?", [account.id],
-          (_, { rows }) => {
-            console.log(rows)
-            setTransaction(rows);
+        tx.executeSql("select * from tansactions where accountid = ?", [id],
+          (tx,results ) => {
+            
+            setTransactions(results.rows._array);
         })
       }
     )
-  
-  }
+}
 
   const renderItem = ({ item }) => {
- 
     return (
       <View>
         <Pressable
-          onPress={() => { updateMain(item) }}>
-          <Text>{item.name}</Text>
+          onPress={() => { updateMain(item) }}
+        
+          style={ [styles.avatar,{borderColor:(item.id===selectedID)?"#FF8157":"#0000",borderWidth:1}]}>
+          <Text style={styles.avatarTitle}>{item.name}</Text>
           </Pressable>
     </View>
     );
-    
+  };
+
+  const chat = ({ item }) => {
+    return (
+      <View style={[ item.side?styles.pchat: styles.rchat, styles.chatCard, styles.flexRow ]}>
+        <View style={styles.flexColumn}>
+        <Text style={styles.chatdesc}>{item.desc}</Text>
+        <Text style={styles.chatdesc}>{ item.date}</Text>
+       </View> 
+        <Text style={styles.chatam}>{ item.amount}</Text>
+
+                </View>
+    );
   };
 
   const initialState = () => {
@@ -203,13 +200,14 @@ console.log(r+p)
     setTotal(null)
     setTpay(null)
     setTreceive(null)
+    setTransactions(null)
   }
 const  delAccount=(id)=>{
   
   db.transaction(
     (tx) => {
       tx.executeSql(`delete from accounts where id = ?;`, [id]);
-      tx.executeSql(`delete from transactions where accountid = ?;`, [id]);
+      tx.executeSql(`delete from tansactions where accountid = ?;`, [id]);
       tx.executeSql("select * from accounts;", [], (tx, results) => {
         setaccounts(results.rows._array),
         initialState();
@@ -243,8 +241,9 @@ const  delAccount=(id)=>{
   
 
   return (
+    
     <View style={styles.container}>
-
+     
       {Platform.OS === "web" ? (
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
@@ -255,7 +254,8 @@ const  delAccount=(id)=>{
         </View>
       ) : (
           <>
-            <View  style={styles.flexColumn}> 
+            <View style={[styles.flexColumn, styles.sidenav]}> 
+            
             <Modal
                 transparent={true}
                 visible={modalVisible}
@@ -268,7 +268,7 @@ const  delAccount=(id)=>{
 
         <View style={[styles.centreModal, styles.flexColumn]}>
                 <View style={styles.modal}>  
-            <Text>Add Account</Text>
+            <Text style={styles.modaltitle}>Add Account</Text>
             <TextInput
                       onChangeText={(text) => setText(text)
                       }
@@ -289,7 +289,7 @@ const  delAccount=(id)=>{
                         }}
                       style={[styles.modalBtn, styles.modalClose]}>
                         {({ pressed }) => (<Text style={{
-                          color: pressed ? "#ffbd59" : "grey",
+                          color: pressed ? "#FF8157" : "darkgrey",
                           fontSize: 16,
                           marginRight:25, }}>Close</Text>)}
                       </Pressable>
@@ -298,9 +298,9 @@ const  delAccount=(id)=>{
                         onPress={() => {
                           add(text);
                           setText(null);}}
-                        style={({ pressed }) => [{backgroundColor:pressed?"darkorange":"#ffbd59"},styles.modalBtn, styles.modalAdd]}
+                        style={({ pressed }) => [{backgroundColor:pressed?"#CF8066":"#FF8157"},styles.modalBtn, styles.modalAdd]}
                       >
-                <Text style={{color:"white"}}>Add 🤗</Text>
+                <Text style={{color:"#EDEDEE"}}>Add 🤗</Text>
               </Pressable>
                   </View>
                 </View>
@@ -314,17 +314,18 @@ const  delAccount=(id)=>{
                      item.id
                   }
                 />
-
+              
+              
               <Pressable
           onPress={() => {
             setmodalVisibal(!modalVisible);
                 }}
-              style={({ pressed }) => [{backgroundColor:pressed?"darkorange":"#ffbd59"},styles.addBtn]}>
-                <Text style={[styles.addBtnText,
-                {color:"white"}]}> + </Text>
+              style={({ pressed }) => [{backgroundColor:pressed?"#FF8157":"#0000"},styles.addBtn]}>
+                {({ pressed }) =>(<Text style={[styles.addBtnText,
+                { color: pressed?"#101820FF":"#FF8157" }]}> + </Text>)}
         </Pressable>
-            </View>
             
+            </View>
 
            <View style={[styles.main, styles.flexColumn]}> 
             <Modal
@@ -339,7 +340,7 @@ const  delAccount=(id)=>{
 
                 <View style={[styles.centreModal, styles.flexColumn]}>
                 <View style={styles.modal}>  
-                      <Text>Add {side?"Payment":"Receiving" } Details</Text>
+                      <Text style={styles.modaltitle}>Add {side?"Payment":"Receiving" } Details</Text>
                   <View style={styles.flexColumn}>
                   <TextInput
                   onChangeText={(desc) => setDesc(desc)}
@@ -372,7 +373,7 @@ const  delAccount=(id)=>{
                           }}
                         style={ [styles.modalBtn, styles.modalClose]}>
                          {({ pressed }) => (<Text style={{
-                          color: pressed ? "#ffbd59" : "grey",
+                         color: pressed ? "#FF8157" : "darkgrey",
                           fontSize: 16,
                           marginRight:25, }}>Close</Text>)}
                         </Pressable>
@@ -381,9 +382,9 @@ const  delAccount=(id)=>{
                             addTransaction(id, desc, amount, side);
                             setAmount(null);
                             setDesc(null);}}
-                        style={({ pressed }) => [{backgroundColor:pressed?"darkorange":"#ffbd59"},styles.modalBtn, styles.modalAdd]}
+                        style={({ pressed }) => [{backgroundColor:pressed?"#CF8066":"#FF8157"},styles.modalBtn, styles.modalAdd]}
                         >
-                    <Text style={{color:"white"}}>Add 💸</Text>
+                    <Text style={{color:"#EDEDEE"}}>Add 💸</Text>
                   </Pressable>
                       </View>
                     </View>
@@ -407,7 +408,7 @@ const  delAccount=(id)=>{
                       }}
                        disabled={id === null?true:false}
                       style={({pressed})=>[styles.delBtn,{backgroundColor:pressed?"#ff5757":"#0000"}]}>
-                      {({ pressed })=>( <Text style={[{ color:pressed?"white":"#ff5757",fontWeight:"bold" }]}>Delete</Text>)}
+                      {({ pressed })=>( <Text style={[{ color:pressed?"white":"#ff5757" }]}>Delete</Text>)}
                      
                     </Pressable>
                   </View>
@@ -422,37 +423,33 @@ const  delAccount=(id)=>{
                             </View>
                             
                           <View style={[styles.chat, styles.flexColumn]}>
-                <Text>CHat area</Text>
+               
+                <FlatList
+                  data={transactions}
+                  renderItem={chat}
+                  keyExtractor={(item)=>item.id}
+                        />
               
-                <View style={ side ?styles.chatRight:styles.chatLeft}>
-                  <Text>date</Text>
-                  <Text>desc</Text>
-                  <Text>amount</Text>
-
-                            </View>
 
                             </View>
                             
               <View style={[styles.mainBottom, styles.flexColumn]}>
                 
-                <View style={styles.flexRow}>
+                <View style={[styles.flexRow,{ marginTop:5}]}>
                   <Text style={{
-                  fontSize: 18,
+                  fontSize: 16,
                 paddingLeft:15,
-                   
-                  marginTop:5,
-                  color: "white",
+             marginTop:2,
+                  color: "#B9B9BA",
                 }}>Net Total: </Text>
                   
                   <Text style={{
-                  fontSize: 26,
+                  fontSize: 19,
                 
-                  color: total===0?"white":total>0?"#7ed975":"#ff5757"
+                  color: total===0?"#CACBCB":total>0?"#41BF5B":"#ff5757"
                 }}> {total} </Text>
                 </View>
 
-               
-            
                           <View style={styles.flexRow}>
 
                   <Pressable
@@ -462,9 +459,11 @@ const  delAccount=(id)=>{
                                     setSide(pay);        
                     }}
                     style={({ pressed })=>[{
-                      backgroundColor:pressed?"red":"#ff5757",
-                   }, styles.Btn]}>
-                              <Text style={{ color:"white"}}>📤 Pay</Text>
+                      backgroundColor: pressed ? "#ff5757" : "#0000",
+                      borderColor: "#ff5757",
+                      borderWidth:1,
+                    }, styles.Btn]}>
+                    {({ pressed })=>(<Text style={{ color:pressed?"white":"#ff5757"}}>Pay</Text>)}
                                 </Pressable>
                                 
                   <Pressable
@@ -474,9 +473,11 @@ const  delAccount=(id)=>{
                                     setSide(receive); 
                     }}
                     style={({ pressed })=>[{
-                      backgroundColor:pressed?"green":"#7ed975",
+                      backgroundColor: pressed ? "#41BF5B" : "#0000",
+                      borderColor: "#41BF5B",
+                      borderWidth:1,
                    }, styles.Btn]}>
-                              <Text style={{ color:"white"}}>📥 Receive</Text>
+                             {({ pressed })=>(<Text style={{ color:pressed?"white":"#41BF5B"}}>Receive</Text>)}
                           </Pressable>
                             </View>
                             </View>
@@ -490,39 +491,48 @@ const  delAccount=(id)=>{
   );
 }
 
-function useForceUpdate() {
-  const [value, setValue] = useState(0);
-  return [() => setValue(value + 1), value];
-}
-
 const styles = StyleSheet.create({
   main: {
     flex:1,
-    
-   
   },
-  
+  sidenav: {
+    backgroundColor: "#101820FF",
+    alignItems: "center",
+    paddingTop:25,
+  },
+  avatar: {
+    backgroundColor:"#2A3036",
+    width: 50,
+    height:50,
+    padding: 5,
+    margin: 8,
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent:"center",
+  },
+
+  avatarTitle: {
+    color: "#CACBCB",
+    fontSize:14,
+  },
+
   mainHeader: {
     flex: 1,
-    backgroundColor: "#545454",
+    backgroundColor: "#1E252C",
     justifyContent: "space-evenly",
     alignItems: "center",
     
   },
-
-  titleSection:{
- 
-},
   
   title: {
     fontSize: 22,
-    color:"white",
+    color:"#CACBCB",
     justifyContent:"space-evenly"
 },
 
   delBtn:{
       borderColor: "#ff5757",
-      borderWidth: 2,
+      borderWidth: 1,
       borderRadius: 4,
         padding: 4,
       marginRight:4,
@@ -542,14 +552,48 @@ const styles = StyleSheet.create({
 },
 
   chat: {
-    height: 100,
     flex: 4,
-    backgroundColor:"#fff"
+    backgroundColor:"#2A3036"
   },
 
+  pchat:{
+    alignSelf:"flex-start"
+    
+  },
+  
+  rchat: {
+    alignSelf:"flex-end"
+  },
+  chatCard: {
+    backgroundColor: "#363C41",
+    margin: 4,
+    padding: 6,
+    borderRadius: 15,
+    justifyContent: "space-between",
+    maxWidth: 210,
+    
+  },
+  chatdesc:{
+    fontSize: 14,
+    color: "#CACBCB",
+    marginHorizontal: 5,
+    paddingHorizontal: 5,
+    flexWrap: "wrap",
+    maxWidth:120,
+  },
+  chatam: {
+    fontSize: 18,
+    color: "#CACBCB",
+    alignSelf: "center",
+    marginHorizontal: 5,
+    paddingHorizontal: 5,
+    paddingLeft:15,
+    borderLeftWidth: 1,
+    borderLeftColor:"grey"
+  },
   mainBottom: {
     flex:1,
-    backgroundColor: "#545454",
+    backgroundColor: "#1E252C",
     justifyContent: "space-evenly",
   },
 
@@ -558,11 +602,15 @@ const styles = StyleSheet.create({
     margin: 10,
     paddingHorizontal: 20,
     flex:1,
-
+    
     alignItems: "center",
     borderRadius: 4,
   },
 
+  modaltitle: {
+  color:"#DCDCDC"
+  },
+  
   container: {
     backgroundColor: "#fff",
     flex: 1,
@@ -573,20 +621,24 @@ const styles = StyleSheet.create({
 
   addBtn: {  
     justifyContent: "center",
-    textAlign: "center",
+    alignItems: "center",
+    alignSelf:"flex-end",
     borderRadius: 50,
     height: 50,
     width: 50,
-    marginHorizontal: 15,
-
+    margin: 15,
+    borderColor: "#FF8157",
+    borderWidth: 1,
+    marginBottom: 28,
+   
+    paddingBottom:10,
   },
 
   addBtnText: {
     fontSize: 35,
-    textAlign: "center",
-    justifyContent: "center",
-    flex: 1,
-    color:"darkslategrey"
+  fontWeight:"200",
+    color: "darkslategrey",
+    
   },
 
   heading: {
@@ -611,9 +663,11 @@ const styles = StyleSheet.create({
 
   modal: {
     margin: 40,
-    backgroundColor: "white",
+    backgroundColor: "#5B5E60",
     borderRadius: 20,
     padding: 30,
+    borderColor: "#DCDCDC",
+    borderWidth:1,
     alignItems: "center",
     shadowColor: "#000",
     shadowOffset: {
@@ -626,7 +680,8 @@ const styles = StyleSheet.create({
 },
 
   input: {
-    borderColor: "#000",
+    backgroundColor:"#DCDCDC",
+    borderColor: "white",
     borderRadius: 4,
     borderWidth: 1,
     marginVertical: 15,
@@ -648,19 +703,4 @@ const styles = StyleSheet.create({
     borderRadius:4,
   },
 
-  listArea: {
-    backgroundColor: "#f0f0f0",
-    width: 70,
-    paddingTop: 16,
-  },
-
-  sectionContainer: {
-    marginBottom: 16,
-    marginHorizontal: 16,
-  },
-
-  sectionHeading: {
-    fontSize: 18,
-    marginBottom: 8,
-  },
 });
